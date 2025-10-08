936aca2aa0e6ec2013232fb93fc21245731c54d9
Lasse Stilvang <lasse.stilvang@gmail.com>
2025-10-08 23:25:16 +0200

    refactor(calendar): remove unused TimeBlock import from WeekView

--- src/components/calendar/WeekView.tsx
+++ src/components/calendar/WeekView.tsx
@@ -1,6 +1,5 @@
 import { useCalendar } from "@/hooks/useCalendar";
 import { CalendarGrid } from "./CalendarGrid";
-import { TimeBlock } from "@/types";
 
 export function WeekView() {
   const { scrollRef, renderedDays, visibleDays, timeBlocks, isScrolling } =
