9294ae6b7871b079ec9ca838aebf17fcbf58673f
Lasse Stilvang <lasse.stilvang@gmail.com>
2025-10-08 23:24:44 +0200

    refactor(calendar): remove unused event handlers from WeekView

--- src/components/calendar/WeekView.tsx
+++ src/components/calendar/WeekView.tsx
@@ -6,19 +6,11 @@ export function WeekView() {
   const { scrollRef, renderedDays, visibleDays, timeBlocks, isScrolling } =
     useCalendar();
 
-  const handleTimeSlotClick = (date: Date) => {
-    // Logic to handle time slot click
-  };
-
-  const handleBlockClick = (block: TimeBlock) => {
-    // Logic to handle block click
-  };
+  
 
   return (
     <CalendarGrid
       timeBlocks={timeBlocks}
-      onTimeSlotClick={handleTimeSlotClick}
-      onBlockClick={handleBlockClick}
       scrollRef={scrollRef}
       renderedDays={renderedDays}
       visibleDays={visibleDays}
